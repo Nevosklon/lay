e7f99129a878fe3f60c07a0f7f627dc2fd12858b
Nevosklon <154698393+Nevosklon@users.noreply.github.com>
2024-08-09 00:53:10 +0800

    [WIP] 'Runtime implmentation'

--- crates/lay-wayland-core/src/lib.rs
+++ crates/lay-wayland-core/src/lib.rs
@@ -2,3 +2,101 @@
 //! lays wayland core
 //! libary that contains the
 //!
+
+use std::{
+    collections::VecDeque,
+    future::Future,
+    io::{IoSlice, IoSliceMut},
+    os::fd::OwnedFd,
+};
+
+use lay_wayland_wire::Header;
+
+// A Blocking event loop using io uring runtime
+pub struct DefaultRuntime {
+    connection: OwnedFd,
+}
+
+// The wayland will use actor model
+pub trait Runtime {
+    type Error;
+    type Buffer;
+    type Handle<'a>: RuntimeHandle;
+
+    const BLOCKING: bool = true;
+    const ASYNC: bool = false;
+
+    /// Blocking constructor of the reactor
+    fn connect_env() -> Option<Self::Buffer>;
+
+    fn read(buffer: &Self::Buffer) -> usize;
+
+    /// async constructor of the reactor
+    fn connecting() -> impl Future<Output = Option<Self::Buffer>> + Send;
+
+    fn reading(buffer: &Self::Buffer) -> impl Future<Output = Option<Self::Error>> + Send;
+
+    fn handle<'a>(&self) -> Self::Handle<'a>;
+}
+
+pub trait RuntimeHandle {
+    type Runtime: Runtime;
+
+    /// Blocking variant of channels sending
+    fn request(&mut self, request: impl Request);
+
+    /// Blocking variant of channels recv
+    fn event(&mut self, event: impl Interface);
+
+    /// async variant of channels sending
+    fn requesting(&self, request: impl Request) -> impl Future<Output = ()>;
+
+    /// async variant of channels recv
+    fn eventing(&self, event: impl Interface) -> impl Future<Output = ()>;
+}
+
+pub mod uring;
+
+pub trait Interface {
+    type Event;
+    type Error;
+}
+
+pub trait Bytes {
+    const FIXED: bool = false;
+    const N: usize = 0;
+}
+
+impl<const N: usize> Bytes for [u8; N] {
+    const FIXED: bool = true;
+    const N: usize = N;
+}
+
+impl Bytes for Vec<u8> {}
+impl Bytes for VecDeque<u8> {}
+impl Bytes for [u8] {}
+impl<'a> Bytes for IoSlice<'a> {}
+impl<'a> Bytes for IoSliceMut<'a> {}
+
+pub trait Request: Sized {
+    type Interface: Interface;
+
+    const N: usize = size_of::<Header>() + size_of::<Self>();
+    const FIXED: bool = const { Self::N > 1 };
+    type Bytes: Bytes;
+
+    fn as_bytes(&self) -> &Self::Bytes;
+    fn into_bytes(&self) -> Self::Bytes;
+    fn request(&self, runtime: &impl RuntimeHandle);
+    fn requesting(&self, runtime: &impl RuntimeHandle) -> impl Future<Output = ()>;
+}
+
+#[macro_export]
+macro_rules! err {
+    ($err:path, $($f:tt)*) => {
+       match $($f)* {
+           Ok(r) => Ok(r),
+           Err(_) => Err($err),
+       }
+    };
+}
---
+++ crates/lay-wayland-core/src/uring.rs
@@ -0,0 +1,106 @@
+use std::{
+    env,
+    ffi::OsStr,
+    io::{BufReader, ErrorKind},
+    os::fd::{FromRawFd, IntoRawFd, OwnedFd},
+    path::Path,
+};
+
+use crate::{DefaultRuntime, Runtime};
+use rustix::{
+    self,
+    fd::{AsFd, AsRawFd, RawFd},
+    io::{Errno, FdFlags},
+    net::{AddressFamily, SocketAddrUnix, SocketFlags, SocketType},
+};
+
+const WAYLAND_SOCKET: &'static str = "WAYLAND_SOCKET";
+
+#[repr(u16)]
+pub enum ConnError {
+    InvalidFD,
+    PermissionDenied,
+    NotFound,
+    IoError { errno: Errno },
+}
+
+impl DefaultRuntime {
+    pub fn from_env() -> Result<Self, ErrorKind> {
+        #[cfg(debug_assertions)]
+        if let Some(socket_fd) = env::var_os(WAYLAND_SOCKET) {
+            log::warn!(target: "connection", "Attempting to connect with WAYLAND_SOCKET");
+
+            let socket_fd: OwnedFd = match socket_fd.to_str() {
+                Some(socket) => unsafe {
+                    OwnedFd::from_raw_fd(socket.parse().map_err(|_| ErrorKind::Other)?)
+                },
+                None => Err(ErrorKind::Other)?,
+            };
+
+            let flags = rustix::io::fcntl_getfd(socket_fd.as_fd()).map_err(|err| err.kind())?;
+
+            rustix::io::fcntl_setfd(socket_fd.as_fd(), flags | FdFlags::CLOEXEC)
+                .map_err(|err| err.kind())?;
+
+            return Ok(Self {
+                connection: socket_fd,
+            });
+        };
+
+        log::info!(target: "connection", "Attempt to connecting to wayland socket");
+
+        if let Some(xdg_path) = env::var_os("XDG_RUNTIME_DIR") {
+            //
+            // XDG_RUNTIME_DIR + WAYLAND_DISPLAY
+            if let Some(display) = env::var_os("WAYLAND_DISPLAY") {
+                let path = Path::new(&xdg_path).join(display);
+                log::info!(target:
+                    "connection",
+                    "Attempting to connect with XDG_RUNTIME + WAYLAND_DISPLAY: {path:?}",
+                );
+
+                if let Ok(connection) = Self::from_file(path) {
+                    return Ok(connection);
+                }
+            }
+
+            let path = Path::new(&xdg_path).join("wayland-0");
+            log::info!(target: "connection", "Attempting to connect with XDG_RUNTIME_DIR + wayland-0: {path:?}");
+            //
+            // Assume that wayland socket is called wayland-0
+            // XDG_RUNTIME_DIR + wayland-0
+            if let Ok(connection) = Self::from_file(Path::new(&xdg_path)) {
+                return Ok(connection);
+            }
+        };
+
+        log::error!(target: "connection", "Unable to connect to wayland socket");
+        Err(ErrorKind::NotFound)
+    }
+
+    pub fn from_file(path: impl AsRef<OsStr>) -> std::io::Result<Self> {
+        let sockfd = rustix::net::socket_with(
+            AddressFamily::UNIX,
+            SocketType::STREAM,
+            SocketFlags::NONBLOCK | SocketFlags::CLOEXEC,
+            None,
+        )?;
+
+        rustix::net::connect_unix(sockfd.as_fd(), &(SocketAddrUnix::new(path.as_ref())?))?;
+
+        return Ok(Self { connection: sockfd });
+    }
+
+    // SAFETY: The File MUST be open and a valid unix domain socket
+    #[inline(always)]
+    pub unsafe fn from_fd(fd: impl IntoRawFd) -> Self {
+        Self {
+            connection: OwnedFd::from_raw_fd(fd.into_raw_fd()),
+        }
+    }
+}
+
+#[test]
+fn connection() {
+    let conn = DefaultRuntime::from_env().unwrap();
+}
